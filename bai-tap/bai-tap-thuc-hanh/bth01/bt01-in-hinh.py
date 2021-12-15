68988b5be31b761170f90c0fe40a860d0f36eb44
Nguyen-Hoang-Thuan-OU <nguyenhoangthuan1409@gmail.com>
2021-12-15 16:35:42 +0700

    update bth01-bt01

--- bai-tap/bai-tap-thuc-hanh/bth01/bt01-in-hinh.py
+++ bai-tap/bai-tap-thuc-hanh/bth01/bt01-in-hinh.py
@@ -1,22 +1,35 @@
 # Cho người dùng nhập số nguyên n và xuất các hình
 
-n = int(input("Nhập n = "))
-print()
+# Câu a - số * theo biến n
+def hinh_vuong(n):
+    print("Hình vuông:")
+    print(("* " * n + "\n")*n)
 
-# Câu a
-print(("*" * n + "\n")*n)
-
-# Câu b
-for i in range(1, n + 1):
-    print("*" * i)
-print()
+# Câu b - số * theo biến i
+def tam_giac_vuong_thuong(n):
+    print("Hình tam giác vuông thường:")
+    for i in range(1, n + 1):
+        print("*" * i)
+    print()
 
 # Câu c
-for i in range(1, n + 1):
-    print(" " * (n - i) + "*" * i)
-print()
+def tam_giac_vuong_nguoc(n):
+    print("Hình tam giác vuông ngược:")
+    for i in range(1, n + 1):
+        print(" " * (n - i) + "*" * i)
+    print()
 
 # Câu d
-for i in range(1, n + 1, 2):
-    print(("*" * i).center(n))
-print()
+def tam_giac_deu_le(n):
+    print("Hình tam giác đều (lẻ):")
+    for i in range(1, n + 1, 2):
+        print(("*" * i).center(n))
+    print()
+
+# Nhập và gọi hàm
+n = int(input("Nhập n = "))
+print()   
+hinh_vuong(n)
+tam_giac_vuong_thuong(n)
+tam_giac_vuong_nguoc(n)
+tam_giac_deu_le(n)
