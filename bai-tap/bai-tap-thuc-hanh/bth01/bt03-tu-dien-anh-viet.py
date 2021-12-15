9aac25e4b6cd82a0a10ece6d723dcf0540b7e115
Nguyen-Hoang-Thuan-OU <nguyenhoangthuan1409@gmail.com>
2021-12-15 23:27:16 +0700

    update bth01-bt03

--- bai-tap/bai-tap-thuc-hanh/bth01/bt03-tu-dien-anh-viet.py
+++ bai-tap/bai-tap-thuc-hanh/bth01/bt03-tu-dien-anh-viet.py
@@ -1,15 +1,15 @@
 # Viết chương trình thực hiện việc xử lý từ điển Anh – Việt
 
 # Câu a: Thêm một từ mới vào từ điển
-def them_tu(tuDien, tu, yNghia):
-    if tu not in tuDien:
-        tuDien[tu] = yNghia
+def them_tu(tuDien, tuGoc, yNghia):
+    if tuGoc not in tuDien:
+        tuDien[tuGoc] = yNghia
 
 # Câu b: Hiển thị từ điển,
 #           cho biết từ điển hiện tại cho bao nhiêu từ
 def hien_thi_tu_dien(tuDien):
-    for tu, yNghia in tuDien.items():
-        print("%s = %s" % (tu, yNghia))
+    for tuGoc, yNghia in tuDien.items():
+        print("%s = %s" % (tuGoc, yNghia))
 
 print("\n======================================\n")
 
@@ -29,31 +29,30 @@ print("\n======================================\n")
 # Câu c: Tìm kiếm từ tiếng Anh,
 #           nếu tìm thấy thì hiển thị key và value.
 #           Nếu không tìm thấy thì thông báo không tìm thấy
-def tim_kiem_tu(tuDien, tu):
-    if tu in tuDien:
-        return tuDien[tu]
+def tim_kiem_tu(tuDien, tuGoc):
+    if tuGoc in tuDien:
+        return tuDien[tuGoc]
     return None
 hien_thi_tu_dien(tuDien)
 
 print("\n======================================\n")
 
-tu = input("Nhập từ cần tìm: ")
-ket_qua = tim_kiem_tu(tuDien, tu)
+tuGoc = input("Nhập từ cần tìm: ")
+ket_qua = tim_kiem_tu(tuDien, tuGoc)
 
 if ket_qua:
     print("Nghĩa của từ là:", ket_qua)
 else:
     print("Không có từ cần tìm!")
 
-print("\n======================================\n")
-
 # Câu d: Xoá một từ trong từ điển dựa trên key cung cấp
-def xoa_tu(tuDien, tu):
-    if tu in tuDien:
-        del tuDien[tu]
+def xoa_tu(tuDien, tuGoc):
+    if tuGoc in tuDien:
+        del tuDien[tuGoc]
 
 print("\n======================================\n")
 
+print("Từ điển sau khi xoá từ:")
 xoa_tu(tuDien, "table")
 hien_thi_tu_dien(tuDien)
 
