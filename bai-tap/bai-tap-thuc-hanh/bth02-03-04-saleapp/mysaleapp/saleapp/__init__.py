7de857a875e674ffd7ab166f73702118a4fabfc4
Nguyen-Hoang-Thuan-OU <nguyenhoangthuan1409@gmail.com>
2021-12-18 11:23:08 +0700

    update secret key

--- bai-tap/bai-tap-thuc-hanh/bth02-03-04-saleapp/mysaleapp/saleapp/__init__.py
+++ bai-tap/bai-tap-thuc-hanh/bth02-03-04-saleapp/mysaleapp/saleapp/__init__.py
@@ -8,7 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] =\
     'mysql+pymysql://root:1234@localhost/saledb?charset=utf8mb4'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.secret_key =\
-    'UwU$40FD#IUO^OPXZC%NK^*23SN-_-M4MDFNB#SD62UvU3$GBXCVM%OwO'
+    'UwU$40FD#IUO^OPXZC%NK^*23SN-_-MFB#SD62UvU3$GBXCVM%OwO'
 
 db = SQLAlchemy(app=app)
 
